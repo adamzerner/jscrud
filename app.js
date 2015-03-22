fb45936ba0f9244359c7eb2eccc72b0fb959fc79
Adam Zerner <azerner3@gmail.com>
2015-03-22 12:34:24 -0400

    list

--- app.js
+++ app.js
@@ -1,109 +1,81 @@
 $(document).ready(function() {
-  // SELECTORS
-  // navs
-  var $listNav = $('#listNav'),
-      $createNav = $('#createNav'),
-  // sections
-      $listSection = $('section.list'),
-      $itemSection = $('section.item'),
-      $createSection = $('section.create'),
-      $editSection = $('section.edit'),
-  // forms
-      $createForm = $("form[name='todoFormCreate']"),
-      $editForm = $("form[name='todoFormEdit']"),
-  // inputs
-      $createInput = $('section.create input'),
-      $editInput = $('section.edit input');
 
-	// VARIABLES
-	var currentPage = 'list',
-      todos = [],
-      currIndex;
+var counter = 0;
 
-	// INITIALIZATION
-	$itemSection
-    .add($createSection)
-    .add($editSection)
-    .hide();
+function Todo(title) {
+  this.id = counter++;
+  this.title = title;
+  this.completed = false;
+  this.dateAdded = Date.now();
+}
 
-	// ROUTING
-	$listNav.on('click', renderList);
-
-	$createNav.on('click', function() {
-    renderPage('create');
-	});
-
-	$('.container').on('click', '#edit', function(e) {
-		currIndex = $(e.target).data('i');
-    $editInput.val(todos[currIndex].val);
-    renderPage('edit');
-	});
-
-  $('.container').on('click', '#delete', function(e) {
-    var i = $(e.target).data('i');
-    todos.splice(i, 1);
-    renderList();
-  });
-
-  function getPage(page) {
-    switch(page) {
-      case 'list': 
-        return $listSection;
-      case 'item': 
-        return $itemSection;
-      case 'create': 
-        return $createSection;
-      case 'edit': 
-        return $editSection;
+var model = {
+  init: function() {
+    if (!localStorage.todos) {
+      localStorage.todos = JSON.stringify([]);
     }
+  },
+  add: function(title) {
+    var todos = JSON.parse(localStorage.todos);
+    var todo = new Todo(title);
+    todos.push(todo);
+    localStorage.todos = JSON.stringify(todos);
+  },
+  remove: function(id) {
+    var todos = JSON.parse(localStorage.todos);
+    for (var i = 0, len = todos.length; i < len; i++) {
+      if (todos[i].id === id) {
+        todos.splice(i, 1);
+        localStorage.todos = JSON.stringify(todos);
+        break;
+      }
+    }
+  },
+  getAll: function() {
+    return JSON.parse(localStorage.todos);
   }
+};
 
-  function renderPage(page) {
-    getPage(currentPage).hide();
-    getPage(page).show();
-    currentPage = page;
+var view = {
+  init: function() {
+    var $ul = $('.list-group'),
+        todos = controller.getTodos(),
+        $li;
+    for (var i = 0, len = todos.length; i < len; i++) {
+      $x = $('<span>&times;</span>')
+      $x
+        .data('id', i);
+
+      $checkbox = $('<input>');
+      $checkbox
+        .attr('type', 'checkbox');
+
+
+      $li = $('<li></li>');
+      $li
+        .addClass('list-group-item')
+        .text(' ' + todos[i].title + ' ')
+        .prepend($checkbox)
+        .append($x);
+      
+      $ul.append($li);
+    }
+  },
+  render: function() {
+
   }
+};
 
-	function renderList() {
-		// render DOM using todos
-    var checkedText;
-		$('.list ul').html('');
-		for (var i = 0, len = todos.length; i < len; i++) {
-      checkedText = todos[i].checked ? 'uncheck' : 'check';
-			$('.list ul').append([
-				'<li>' + todos[i].val + 
-          ' | ' +  '<a id="check" data-i="' + i + '">' + checkedText + '</a>' + 
-					' | ' +  '<a id="edit" data-i="' + i + '">edit</a>' + 
-					' | ' +  '<a id="delete" data-i="' + i + '">delete</a>' + 
-				'</li>'
-			].join());
-		}
-    renderPage('list');
-	}
+var controller = {
+  init: function() {
+    model.init();
+    view.init();
+  },
+  getTodos: function() {
+    return model.getAll();
+  }
+};
 
-	// FORMS
-	$createForm.on('submit', function(e) {
-		e.preventDefault();
-		todos.push({
-      val: $createInput.val(),
-      checked: false
-    });
-		$createInput.val('');
-		renderList();
-	});
+controller.init();
 
-	$editForm.on('submit', function(e) {
-		e.preventDefault();
-		var newVal = $editInput.val();
-    todos[currIndex].val = newVal;
-		$editInput.val('');
-		renderList();
-	});
-
-  // CHECKING
-  $('.container').on('click', '#check', function(e) {
-    var i = $(e.target).data('i');
-    todos[i].checked = !todos[i].checked;
-    renderList();
-  });
 });
